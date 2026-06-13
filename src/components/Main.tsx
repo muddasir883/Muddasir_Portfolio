f316242e8867d302638c2f3863307e6de89b4fb6
muddasir883 <muhammadmuddasir883@gmail.com>
2026-06-13 17:24:24 +0500

    Initial commit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
-
+import myAvatar from '../assets/img.jpeg';
 function Main() {
 
   return (
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <div className="image-wrapper">
+            <img src={myAvatar} alt="Avatar" />
+          </div>
         </div>
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/muddasir883" target="_blank" rel="noreferrer"><GitHubIcon /></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            <a href="" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
           </div>
           <h1>Muddasir</h1>
           <p>Sofware Engineer</p>
